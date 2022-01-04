073b8a413398a14ec27e96b3bf5a8946a3c3f9da
WaaZiiX <96393635+WaaZiiX03@users.noreply.github.com>
2022-01-04 18:56:50 +0100

    Add files via upload
    Bonjour/Bonsoir,
    
    Aujourd'hui on vous présente un serveur Discord pour l'aide au développement, que ce soit pour FiveM ou pour Discord. Ainsi nous pouvons vous aider à corriger les scripts, les erreurs (de votre serveur), entre autre. Différents channels sont mis en place afin de pouvoir vous partager certain scripts et mapping, vérifié au préalable.
    
    En nous rejoignant vous trouverez :
    
     => Des scripts en tous genre optimisés et sans erreurs.
     => De nombreuses créations.
     => Des mappings.
     => De l'aide pour la création de bases, de bots discord, mais également pour apprendre à dev' (FiveM et Discord).
     => De l'aide communautaire.
     => Possibilité d'être partenaires.
     => Partages de scripts fonctionnels et sans bugs.
     => Créations de cores.
     => Créations de gangs/orga's AVEC le système de coffre d'argent sale.
     
    
    Tous scripts présents et à votre disponibilité a d'abord été testé dans sa totalité et rectifié par notre équipe.
    Nous sommes également disponible en vocal et pas uniquement par écrit. On souhaite aider avant tout, et discuter directement est parfois bien plus simple et plus rapide que par écrit! 
    
    Si vous souhaitez avoir ce petit coup de pouce bénévole, n'hésitez pas à nous rejoindre sur le serveur en cliquant sur ce lien : https://discord.gg/YcuCQEaKFD

---
+++ config.lua
@@ -0,0 +1,22 @@
+Concess             = {}
+
+Concess.webhooks = "https://discord.com/api/webhooks/896744378603798558/bkLFuY2O5sWp3m-cYjAYE-ULB9rVZSyxBjjz84VDjDZEcbCv-xeC1QdpFhfeSrVeWCVW"
+
+Concess.jeveuxmarker = true --- true = Oui | false = Non
+
+Concess.jeveuxblips = true --- true = Oui | false = Non
+
+Concess.pos = {
+	menu = {
+		position = {x = -781.288, y = -211.406, z = 37.15}
+	},
+	boss = {
+		position = {x = -810.071, y = -208.652, z = 37.12}
+	},
+	serrurier = {
+		position = {x = -776.713, y = -238.125, z = 37.14}
+	},
+	spawnvoiture = {
+		position = {x = -791.752, y = -217.492, z = 37.41, h = 210.959}
+	},
+}
